c77ec3760fe2ea71d9bfb4ef5e9bce3423bf16cd
UnknownShadow200 <unknownshadow200@gmail.com>
2026-06-27 20:13:52 +1000

    Refactor Matcher class

--- MCGalaxy/util/Formatting/Matcher.cs
+++ MCGalaxy/util/Formatting/Matcher.cs
@@ -20,7 +20,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace MCGalaxy 
-{  
+{
     /// <summary> Finds partial matches of a 'name' against the names of the items in an enumerable. </summary>
     /// <remarks> returns number of matches found, and the matching item if only 1 match is found. </remarks>
     public static class Matcher 
@@ -78,49 +78,72 @@ namespace MCGalaxy
             return Find<T>(p, name, out matches, items, filter, nameGetter, nameGetter, group, limit);
         }
         
-        /// <summary>
-        /// Binary compatibility with plugins before feedback was introduced
-        /// </summary>
-        public static T Find<T>(Player p, string name, out int matches, IEnumerable<T> items,
-                                Predicate<T> filter, StringFormatter<T> nameGetter, 
-                                StringFormatter<T> itemFormatter, string group, int limit = 5) {
-            return Find<T>(p, name, out matches, items, filter, nameGetter, itemFormatter, group, limit, true);
-        }
         /// <summary> Finds partial matches of 'name' against the names of the items in the 'items' enumerable. </summary>
         /// <returns> If exactly one match, the matching item. </returns>
         public static T Find<T>(Player p, string name, out int matches, IEnumerable<T> items,
                                 Predicate<T> filter, StringFormatter<T> nameGetter, 
-                                StringFormatter<T> itemFormatter, string group, int limit, bool feedback)  {
-            T match = default(T); matches = 0;
-            StringBuilder output = new StringBuilder();
-            const StringComparison comp = StringComparison.OrdinalIgnoreCase;
+                                StringFormatter<T> itemFormatter, string group, int limit = 5) {
+            List<T> results = GetMatches(name, items, filter, nameGetter, limit);
+            matches = results.Count;
+            
+            PrintMatches(p, name, results, itemFormatter, group, limit);
+            return matches == 1 ? results[0] : default(T);
+        }
+        
+        
+        /// <summary> Finds partial matches of 'name' against the names of the items in the 'items' enumerable. </summary>
+        /// <returns> Matching items, with exact match taking priority </returns>
+        public static List<T> GetMatches<T>(string name, IEnumerable<T> items, Predicate<T> filter, 
+                                            StringFormatter<T> nameGetter, int limit)  {
+            List<T> matches = new List<T>(limit + 1);
+            const StringComparison CASELESS = StringComparison.OrdinalIgnoreCase;
 
             foreach (T item in items)
             {
                 if (filter != null && !filter(item)) continue;
                 string itemName = nameGetter(item);
-                if (itemName.Equals(name, comp)) { matches = 1; return item; }
-                if (itemName.IndexOf(name, comp) < 0) continue;
+                if (itemName.IndexOf(name, CASELESS) < 0) continue;
                 
-                match = item; matches++;
-                if (matches <= limit) {
+                // Exact match takes priority over partial matches
+                if (itemName.Equals(name, CASELESS)) { 
+                    matches.Clear();
+                    matches.Add(item);
+                    return matches;
+                }
+                     
+                if (matches.Count <= limit) matches.Add(item);
+            }
+            return matches;
+        }
+        
+        // TODO matchresult class maybe?
+        public static void PrintMatches<T>(Player p, string name, List<T> results,
+                                           StringFormatter<T> itemFormatter, string group, int limit) {
+            int matches = results.Count;
+            
+            if (matches == 0) {
+                p.Message("No {0} match \"{1}\".", group, name); 
+                return;
+            }
+            if (matches == 1) return;
+            
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < matches; i++)
+            {
+                T item = results[i];
+                
+                if (i < limit) {
                     output.Append(itemFormatter(item)).Append("&S, ");
-                } else if (matches == limit + 1) {
+                } else {
                     output.Append("(and more), ");
                 }
-            }
-            
-            if (matches == 1) return match;
-            if (matches == 0) {
-                if (feedback) p.Message("No {0} match \"{1}\".", group, name); return default(T);
             }
             
             string count = matches > limit ? limit + "+ " : matches + " ";
             string names = output.ToString(0, output.Length - 2);
             
-            if (feedback) p.Message("{0}{1} match \"{2}\":", count, group, name);
-            if (feedback) p.Message(names);
-            return default(T);
+            p.Message("{0}{1} match \"{2}\":", count, group, name);
+            p.Message(names);
         }
     }
 }
