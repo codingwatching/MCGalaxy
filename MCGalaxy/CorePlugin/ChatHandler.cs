9d36fe586764025b006a26107fa5d7a91a12379d
Goodlyay <zackxv7@yahoo.com>
2026-07-13 11:11:40 -0700

    /Pony and /rainbowdashlikescoolthings account for pronouns

--- MCGalaxy/CorePlugin/ChatHandler.cs
+++ MCGalaxy/CorePlugin/ChatHandler.cs
@@ -39,6 +39,11 @@ namespace MCGalaxy.Core {
             if (scope != ChatScope.PM) Logger.Log(logType, text);
         }
         
+        static string PonyName(Player p) {
+            if (p.pronouns.ThirdPersonObjectiveSingular.CaselessEq("him")) return "Brony";
+            if (p.pronouns.ThirdPersonObjectiveSingular.CaselessEq("her")) return "Pegasister";
+            return "Pony";
+        }
         internal static void HandleCommand(Player p, string cmd, string args, CommandData data) {
             if (!Server.Config.CoreSecretCommands) return;
 
@@ -47,12 +52,12 @@ namespace MCGalaxy.Core {
                 p.cancelcommand = true;
                 if (!MessageCmd.CanSpeak(p, cmd)) return;
                 int used = p.Extras.GetInt("MCG_PONY");
-                            
+
                 if (used < 2) {
-                    Chat.MessageFrom(p, "λNICK &Sjust so happens to be a proud brony! Everyone give λNICK &Sa brohoof!");
+                    Chat.MessageFrom(p, "λNICK &Sjust so happens to be a proud " + PonyName(p).ToLower() + "! Everyone give " + p.pronouns.ThirdPersonObjectiveSingular + " &Sa brohoof!");
                     Logger.Log(LogType.CommandUsage, "{0} used /{1}", p.name, cmd);
                 } else {
-                    p.Message("You have used this command 2 times. You cannot use it anymore! Sorry, Brony!");
+                    p.Message("You have used this command 2 times. You cannot use it anymore! Sorry, {0}!", PonyName(p));
                 }
                 
                 p.Extras["MCG_PONY"] = used + 1;
@@ -65,7 +70,7 @@ namespace MCGalaxy.Core {
                     Chat.MessageGlobal("&4T&6H&eI&aS&3 S&9E&1R&4V&6E&eR &aJ&3U&9S&1T &4G&6O&eT &a2&30 &9P&1E&4R&6C&eE&aN&3T &9C&1O&4O&6L&eE&aR&3!");
                     Logger.Log(LogType.CommandUsage, "{0} used /{1}", p.name, cmd);
                 } else {
-                    p.Message("You have used this command 2 times. You cannot use it anymore! Sorry, Brony!");
+                    p.Message("You have used this command 2 times. You cannot use it anymore! Sorry, {0}!", PonyName(p));
                 }
                 
                 p.Extras["MCG_RD"] = used + 1;
